b474255a259f8e9303c2953b8a4dcdafbe69ff64
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-13 22:14:39 +0100

    added more menu items

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -6,8 +6,29 @@ const Homepage = () => (
       <div className='menu-item'>
         <div className='content'>
           <h1 className='title'>HATS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>JACKETS</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>WOMEN</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>MEN</h1>
+          <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
     </div>
   </div>
 )
+
+export default Homepage
