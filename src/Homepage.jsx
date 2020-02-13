44f9af2c0abc4cbb4e6a79cb81896de2c8dce907
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-13 22:04:08 +0100

    started creating Homepage component

---
+++ src/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>HATS</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+)
