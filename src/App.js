50a3096e66194c8166dda14b7592c4215ddca710
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-14 00:06:59 +0100

    added some Router parts

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Homepage from './pages/homepage/Homepage'
-import { Switch } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/hats' component={Homepage} />
       </Switch>
     </div>
   )
