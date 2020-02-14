84bcc0797cf5e9bcffb40486c7d50bdd62805b5e
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-14 21:18:08 +0100

    render CollectionItem

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -8,8 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map(({ id, ...itemProps }) => (
+          <CollectionItem key={id} {...itemProps} />
         ))}
     </div>
   </div>
