13fcaf84a8bdd75daab09d1ef3fd6b45ccd68c40
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-14 21:48:52 +0100

    render Header inside App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import './App.css'
-
 import Homepage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/ShopPage'
+import Header from './components/Header/Header'
 import { Switch, Route } from 'react-router-dom'
+
+import './App.css'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
