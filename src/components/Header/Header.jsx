7dca04ffcf45bfd383682605fcf803dcbac3201c
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-14 21:36:23 +0100

    added Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import './Header.styles.scss'
 
-const Header = () => <div></div>
+const Header = () => (
+  <div className='header'>
+    <Link to='/'></Link>
+  </div>
+)
 
 export default Header
