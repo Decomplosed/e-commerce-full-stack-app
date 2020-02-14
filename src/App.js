10368545dfbe2b4b1e431f9b54de7e47412cc9d3
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-14 23:21:37 +0100

    import SignInAndSignUp component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Homepage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/ShopPage'
+import SignInAndSignUp from './pages/sign-in-and-sing-up/SignInAndSignUp'
 import Header from './components/Header/Header'
 import { Switch, Route } from 'react-router-dom'
 
