9a05c35772a85b3334176aa7f0068ec5d892a5fd
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-14 23:33:40 +0100

    render SignIn inside SignInAndSignUp

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import './SignIn.scss'
+import './SignIn.styles.scss'
 
 class SignIn extends React.Component {
   constructor(props) {
--- src/pages/sign-in-and-sing-up/SignInAndSignUp.jsx
+++ src/pages/sign-in-and-sing-up/SignInAndSignUp.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import SignIn from '../../components/SignIn/SignIn'
 
-import './SignInAndSignUp.scss'
+import './SignInAndSignUp.styles.scss'
 
-const SignInAndSignUp = () => <div className='sign-in-and-sign-up'>SIGN IN</div>
+const SignInAndSignUp = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+  </div>
+)
 
 export default SignInAndSignUp
