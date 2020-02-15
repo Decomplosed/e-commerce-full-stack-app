f08fd043334e6904b29b7f52ba521f5d3fdaf297
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-15 23:42:09 +0100

    call it once user signed in with google

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      createUserProfileDocument(user)
     })
   }
 
