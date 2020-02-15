b5637de13fd902ea8305e64c83553460dcefb87e
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-15 22:21:12 +0100

    added constructor

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
   render() {
     return (
       <div>
