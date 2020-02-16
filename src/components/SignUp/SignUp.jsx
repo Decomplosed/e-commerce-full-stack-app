8a2f2169b6c6ba9d499c33e06a8a9fc20f8f37d0
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-16 13:26:40 +0100

    import styles and start creating a class based component of SignUp which will store the user details inside state

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,3 +3,18 @@ import FormInput from '../FormInput/FormInput'
 import CustomButton from '../CustomButton/CustomButton'
 
 import { auth, createUserProfilDocument } from '../../firebase/firebase.utils'
+
+import './SignUp.styles.scss'
+
+class SignUp extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      dispalyName: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  }
+}
