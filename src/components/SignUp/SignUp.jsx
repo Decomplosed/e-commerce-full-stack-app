f413894b1f92ff8acf4d543bdd64f4169208e778
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-16 13:25:11 +0100

    import firebase part

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import FormInput from '../FormInput/FormInput'
 import CustomButton from '../CustomButton/CustomButton'
 
-
+import { auth, createUserProfilDocument } from '../../firebase/firebase.utils'
