4e3ff3530cfb00a1fbbce1d3b60f8042e1230edb
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-18 18:03:41 +0100

    implemented itemCount to the template so now there is item count showing inside shop bag icon

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -5,10 +5,10 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 import './CartIcon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 )
 
