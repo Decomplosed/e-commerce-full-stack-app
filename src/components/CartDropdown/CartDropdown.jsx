b92273f9f7488896b607c4c7e349f973a061fdae
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-19 16:19:14 +0100

    import toggleCartHidden from cart actions and extract dispatch inside the function

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -3,13 +3,14 @@ import CustomButton from '../CustomButton/CustomButton'
 import CartItem from '../CartItem/CartItem'
 
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 import { withRouter } from 'react-router-dom'
 
 import './CartDropdown.styles.scss'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
