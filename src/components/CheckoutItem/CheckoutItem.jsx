2c08aaf67e012c9b6d516ff970a8c08f80f6cd8e
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-19 17:11:42 +0100

    import removeItem action

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions'
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem
+} from '../../redux/cart/cart.actions'
 
 import './CheckoutItem.styles.scss'
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem
   return (
     <div className='checkout-item'>
