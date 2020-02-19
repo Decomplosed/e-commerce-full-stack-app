6e12c6bb5aeaa3cf6a327c3c28a41d651a540e8f
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-19 15:48:51 +0100

    imported withRouter, wrapped whole export inside it and added onCLick event to the button which moves to the /checkout route

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -5,10 +5,11 @@ import CartItem from '../CartItem/CartItem'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+import { withRouter } from 'react-router-dom'
 
 import './CartDropdown.styles.scss'
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -19,7 +20,9 @@ const CartDropdown = ({ cartItems }) => (
         <span className='empty-message'>Your cart is empty</span>
       )}
     </div>
-    <CustomButton>GO TO CHECKOUT</CustomButton>
+    <CustomButton onClick={() => history.push('/checkout')}>
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 )
 
@@ -27,4 +30,4 @@ const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems
 })
 
-export default connect(mapStateToProps)(CartDropdown)
+export default withRouter(connect(mapStateToProps)(CartDropdown))
