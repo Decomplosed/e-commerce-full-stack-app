a6211db54b4d2358cbdf614705a14b0d3f1c6e0a
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-19 15:54:17 +0100

    imported needed methods from react and reselect

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 import './Checkout.styles.scss'
 
