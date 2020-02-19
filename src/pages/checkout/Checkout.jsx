6b9b2241336e881674672d77becd467d114ed4ce
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-19 16:40:43 +0100

    import CheckoutItem

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CheckoutItem from '../../components/CheckoutItem/CheckoutItem'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
