29f4bafc315b77a63bab89f6409987e5441693b5
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-20 22:20:58 +0100

    wrapped App inside PersistGate

--- src/index.js
+++ src/index.js
@@ -12,7 +12,9 @@ import App from './App'
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
