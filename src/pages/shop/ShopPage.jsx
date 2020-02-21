2270e9d01e7ff1455b2bfbaa1291996ca948cf8e
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-21 18:48:30 +0100

    changed ShopPage to be functional component instead of class one

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,26 +1,12 @@
 import React from 'react'
 import CollectionPreview from '../../components/CollectionPreview/CollectionPreview'
-import SHOP_DATA from './shop.data'
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...collectionProps }) => (
-          <CollectionPreview key={id} {...collectionProps} />
-        ))}
-      </div>
-    )
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    {collections.map(({ id, ...collectionProps }) => (
+      <CollectionPreview key={id} {...collectionProps} />
+    ))}
+  </div>
+)
 
 export default ShopPage
