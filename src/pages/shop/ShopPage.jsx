fa96783349700fc674799947ff9ef05d95abbbbf
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-21 18:51:28 +0100

    import createStructuredSelector and selectShopItems

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import CollectionPreview from '../../components/CollectionPreview/CollectionPreview'
 import { connect } from 'redux'
+import { createStructuredSelector } from 'reselect'
+import { selectShopItems } from '../../redux/shop/shop.selectors'
 
 const ShopPage = ({ collections }) => (
   <div className='shop-page'>
