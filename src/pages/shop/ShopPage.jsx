8ee6289c8a954479b447b0d914c8dcbb59ac3940
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-21 19:25:57 +0100

    started making a Route based on a collection

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -4,7 +4,7 @@ import { Route } from 'react-router-dom'
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <Route exact path={} component={CollectionsOverview} />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
   </div>
 )
 
