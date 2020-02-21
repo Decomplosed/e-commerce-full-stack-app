3f14be5ce01ff8bff794b26b529034c1e8afb6e4
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-21 19:02:26 +0100

    created a component and moved the mapping from ShopPage

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
-
-
 import './CollectionsOverview.styles.scss'
+
+const CollectionsOverview = ({ collections }) => (
+  <div className='collections-oveerview'>
+    {collections.map(({ id, ...collectionProps }) => (
+      <CollectionPreview key={id} {...collectionProps} />
+    ))}
+  </div>
+)
