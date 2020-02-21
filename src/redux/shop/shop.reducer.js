a50ad6499f64bad0a3129b64e4d491e8479bcdf6
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-21 18:45:12 +0100

    created a shopReducer

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,5 +1,13 @@
 import SHOP_DATA from './shop.data'
+import { bindActionCreators } from 'redux'
 
 const INITIAL_STATE = {
   colelctions: SHOP_DATA
+}
+
+const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
 }
