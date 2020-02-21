5843380d31639bfac75c8da8a10215c8334e0746
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-21 19:23:02 +0100

    moved Route import into ShopPage component

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview'
+import { Route } from 'react-router-dom'
 
 const ShopPage = () => (
   <div className='shop-page'>
