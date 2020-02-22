05608430924c93a1c12d616e186aea0c7f83c494
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-22 13:39:24 +0100

    imported selectCollection selector

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import CollectionItem from '../../components/CollectionItem/CollectionItem'
 
 import { connect } from 'react-redux'
+import { selectCollection } from '../../redux/shop/shop.selectors'
 
 import './Collection.styles.scss'
 
