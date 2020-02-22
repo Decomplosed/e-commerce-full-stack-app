6aaaacb93584c06376136bf0ff927abcc197b617
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-22 12:53:22 +0100

    created a nested Route for category

--- src/pages/shop/ShopPage.jsx
+++ src/pages/shop/ShopPage.jsx
@@ -6,6 +6,7 @@ import { Route } from 'react-router-dom'
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
   </div>
 )
 
