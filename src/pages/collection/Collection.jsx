f7abf5bff1485d9e1b35b6549831c75479e7214f
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-22 13:53:15 +0100

    created a template for each collection

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -8,7 +8,16 @@ import './Collection.styles.scss'
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection
-  return <div className='collection-page'>{}</div>
+  return (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 const mapStateToProps = (state, ownProps) => ({
