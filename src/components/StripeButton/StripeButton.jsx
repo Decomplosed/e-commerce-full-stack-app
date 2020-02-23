cfcbf6b0054f56e770f741d027d6f3184ab594f5
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-23 23:09:02 +0100

    set the button with stripe attributes

--- src/components/StripeButton/StripeButton.jsx
+++ src/components/StripeButton/StripeButton.jsx
@@ -5,5 +5,23 @@ const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_kLPFEmHxkHT7AV9o9aq7HAZF00yebZCSv1'
 
-  return <StripeCheckout />
+  const onToken = token => {
+    console.log(token)
+    alert('Payment Succesful')
+  }
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Clothing Shop Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/up/d/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
 }
