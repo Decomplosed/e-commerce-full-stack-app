35b228c10f22306fe6a06ff97012096b5bda2a98
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-23 15:11:46 +0100

    import selectCollectionsForPreview

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CollectionPreview from '../CollectionPreview/CollectionPreview'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 import './CollectionsOverview.styles.scss'
 
