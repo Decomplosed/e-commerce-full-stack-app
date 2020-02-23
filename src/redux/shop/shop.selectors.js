f1c319e16a38e177348cf022eb290b70aa884e77
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-23 15:10:56 +0100

    added selector for collections after changing the shop.data structure

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,7 +7,13 @@ export const selectCollections = createSelector(
   shop => shop.collections
 )
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+)
+
 export const selectCollection = collectionUrlParam =>
-  createSelector([selectCollections], collections =>
-    collections.find(collections[collectionUrlParam])
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
   )
