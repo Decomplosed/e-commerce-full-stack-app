c42cdb6a8dcfa2593a47891b06f3074231ed9249
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-23 23:04:29 +0100

    started creating functional component for StripeCheckout

--- src/components/StripeButton/StripeButton.jsx
+++ src/components/StripeButton/StripeButton.jsx
@@ -1,2 +1,9 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100
+  const publishableKey = 'pk_test_kLPFEmHxkHT7AV9o9aq7HAZF00yebZCSv1'
+
+  return <StripeCheckout />
+}
