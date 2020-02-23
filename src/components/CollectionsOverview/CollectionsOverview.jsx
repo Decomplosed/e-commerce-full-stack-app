9ca490bbcf7fe8e7a87299128d3006bc9b378acb
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-23 15:12:03 +0100

    point collections to selectCollectionsForPreview selector

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -15,7 +15,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionsOverview)
