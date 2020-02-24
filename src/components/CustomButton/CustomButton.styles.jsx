f13890f444350304147bcd97c5648483c0da2f2c
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-24 16:40:35 +0100

    implement getButtonStyles

--- src/components/CustomButton/CustomButton.styles.jsx
+++ src/components/CustomButton/CustomButton.styles.jsx
@@ -49,21 +49,15 @@ export const CustomButtonContainer = styled.button`
   letter-spacing: 0.5px;
   line-height: 50px;
   padding: 0 35px 0 35px;
-  font-size: 15px;
-  background-color: black;
   color: white;
+  font-size: 15px;
   text-transform: uppercase;
   font-family: 'Open Sans Condensed';
   font-weight: bolder;
-  border: none;
   transition: 0.3s ease-in-out;
   cursor: pointer;
   display: flex;
   justify-content: center;
 
-  &:hover {
-    background-color: white;
-    color: black;
-    border: 1px solid black;
-  }
+  ${getButtonStyles}
 `
