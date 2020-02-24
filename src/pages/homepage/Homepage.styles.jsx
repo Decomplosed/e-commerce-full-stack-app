3f5cdccea21220730e91e0ec9f8a96e320d83ccc
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-24 10:28:15 +0100

    fixed typo

--- src/pages/homepage/Homepage.styles.jsx
+++ src/pages/homepage/Homepage.styles.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const HomePageCotnainer = styled.div`
+export const HomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
