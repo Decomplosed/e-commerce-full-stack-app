17ad9a90ba6298771c6c5071a05b32561648330d
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-24 10:26:25 +0100

    export created container

--- src/pages/homepage/Homepage.styles.jsx
+++ src/pages/homepage/Homepage.styles.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const HomePageCotnainer = styled.div`
+export const HomePageCotnainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
