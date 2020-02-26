62c6d5877193f0cdae06382bd4116699d3e075d0
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-26 17:46:41 +0100

    imported thunk from redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { persistStore } from 'redux-persist'
+import thunk from 'redux-thunk'
 
 import rootReducer from './root.reducer'
 
