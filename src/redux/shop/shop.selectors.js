151bc587d9bd5d29d50684306a4e77b354d155d6
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-26 22:47:25 +0100

    created a selector of selectIsColletionLoaded

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -22,3 +22,8 @@ export const selectIsCollectionFetching = createSelector(
   [selectShop],
   shop => shop.isFetching
 )
+
+export const selectIsColletionLoaded = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+)
