7a0c482dc73ace9614199948465c94f0e0576056
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-26 17:56:26 +0100

    removed UPDATE_COLLECTIONS case

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -25,11 +25,6 @@ const shopReducer = (state = INITIAL_STATE, action) => {
         isFetching: false,
         errorMessage: action.payload
       }
-    case ShopActionTypes.UPDATE_COLLECTIONS:
-      return {
-        ...state,
-        collections: action.payload
-      }
     default:
       return state
   }
