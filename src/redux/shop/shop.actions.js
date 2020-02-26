7980925760c57d60d686ab7e3303fa31f79e303b
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-26 18:09:00 +0100

    fixed error message

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -29,6 +29,6 @@ export const fetchCollectionsStartAsync = () => {
         const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
         dispatch(fetchCollectionsSuccess(collectionsMap))
       })
-      .catch(error => dispatch(fetchCollectionsFailure(errorMessage)))
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)))
   }
 }
