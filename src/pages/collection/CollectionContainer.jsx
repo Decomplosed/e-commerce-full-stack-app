985c853d44e6d056c757d6fa715f7242b3ce5509
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 09:35:40 +0100

    made a mapStateToProps

--- src/pages/collection/CollectionContainer.jsx
+++ src/pages/collection/CollectionContainer.jsx
@@ -4,3 +4,8 @@ import { createStructuredSelector } from 'reselect'
 
 import { selectIsColletionLoaded } from '../../redux/shop/shop.selectors'
 import WithSpinner from '../../components/WithSpinner/WithSpinner'
+import CollectionPage from './Collection'
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: state => selectIsColletionLoaded(state)
+})
