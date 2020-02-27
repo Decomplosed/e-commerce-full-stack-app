fb4f8965b3f83915fccfc02ef4c31f8a9edf10fd
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 09:34:25 +0100

    import withSpinner HOC

--- src/pages/collection/CollectionContainer.jsx
+++ src/pages/collection/CollectionContainer.jsx
@@ -3,3 +3,4 @@ import { compose } from 'redux'
 import { createStructuredSelector } from 'reselect'
 
 import { selectIsColletionLoaded } from '../../redux/shop/shop.selectors'
+import WithSpinner from '../../components/WithSpinner/WithSpinner'
