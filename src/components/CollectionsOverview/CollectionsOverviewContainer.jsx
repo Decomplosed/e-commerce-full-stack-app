929c84cb64a15466899d3827abd158483518bf70
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 08:28:43 +0100

    import compose and created a CollectionsOverviewContainer using compose

--- src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
+++ src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
 import WithSpinner from '../WithSpinner/WithSpinner'
 import CollectionsOverview from './CollectionsOverview'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+import { compose } from 'redux'
 
 import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
 
 const mapStateToProps = createStructuredSelector({
   isLoading: selectIsCollectionFetching
 })
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview)
