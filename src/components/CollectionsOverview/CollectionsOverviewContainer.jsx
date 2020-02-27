c590ad6e6ee79d29aec64cd656abda3b1afa7449
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 08:17:03 +0100

    import WithSpinner HOC

--- src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
+++ src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import WithSpinner from '../WithSpinner/WithSpinner'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
-
