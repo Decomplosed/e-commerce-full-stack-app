3f1f1ab221db3da117a7fec8acad89479bf7f48a
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 08:22:54 +0100

    imported CollectionvOverview component and created mapStateToProps method which is getting isLoading value from selectIsCollectionFetching selector

--- src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
+++ src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import WithSpinner from '../WithSpinner/WithSpinner'
+import CollectionsOverview from './CollectionsOverview'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectionFetching
+})
