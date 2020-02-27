473a6a2fc0c0f35bec169a36b67aac6d52e6b619
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 08:16:11 +0100

    import selectIsCollectionFetching selector

--- src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
+++ src/components/CollectionsOverview/CollectionsOverviewContainer.jsx
@@ -1,3 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
+
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
+
