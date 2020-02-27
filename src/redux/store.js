76df5904f6360f0541069d060513a0f4eacde6cd
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-27 10:14:23 +0100

    implement sagaMiddlewares to middlewares array

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './root.reducer'
 
 const sagaMiddleware = createSagaMiddleware()
 
-const middlewares = []
+const middlewares = [sagaMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
