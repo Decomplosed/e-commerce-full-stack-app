efd72ee7e33b1185adf8feaad33c931431587489
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-28 19:38:27 +0100

    created a generator functions to run fetching data asynchronously

--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,3 +1,14 @@
 import { takeEvery } from 'redux-saga/effects'
 
 import ShopActionTypes from './shop.types'
+
+export function* fetchCollectionsAsync() {
+  yield console.log('Fired')
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  )
+}
