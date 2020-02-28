a6c64e3f135f2c3a686efb5ed1d5e1f709768ab4
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-28 23:32:11 +0100

    created action of googleSignInStart

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -4,3 +4,7 @@ export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
   payload: user
 })
+
+export const googleSignInStart = () => ({
+  type: UserActionTypes.GOOGLE_SIGN_IN_START
+})
