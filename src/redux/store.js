c2ca76520c2ed890322c4caa3fcff3a7f118905c
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-28 19:38:46 +0100

    import fetchCollectionsStart

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { persistStore } from 'redux-persist'
 import createSagaMiddleware from 'redux-saga'
+import { fetchCollectionsStart } from './shop/shop.saga'
 
 import rootReducer from './root.reducer'
 
