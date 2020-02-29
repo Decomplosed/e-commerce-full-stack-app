02b5b6fbe27c6486bf5a89b18f49f2da04c9b708
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-29 11:27:33 +0100

    copied google actions and changed them to trigger emails

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -18,3 +18,17 @@ export const googleSignInFailure = err => ({
   type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
   payload: err
 })
+
+export const emailSignInStart = () => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_START
+})
+
+export const emailSignInSuccess = user => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user
+})
+
+export const emailSignInFailure = err => ({
+  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
+  payload: err
+})
