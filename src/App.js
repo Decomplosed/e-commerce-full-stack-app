472fbbb04c8d724b4352b8cab8d2b2859c0ea1f9
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-01 08:57:58 +0100

    imported checkUserSession

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { connect } from 'react-redux'
 
 import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from './redux/user/user.selectors'
+import { checkUserSession } from './redux/user/user.actions'
 
 import './App.css'
 
