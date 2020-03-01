c9f019aa721022369a77c6ed62bcec249b0fbdeb
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-01 08:11:51 +0100

    changed code of user saga to now use signInSuccess and signInFailure

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -15,11 +15,9 @@ export function* signInWithGoogle() {
     const { user } = yield auth.signInWithPopup(googleProvider)
     const userRef = yield call(createUserProfileDocument, user)
     const userSnapshot = yield userRef.get()
-    yield put(
-      googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-    )
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
   } catch (err) {
-    yield put(googleSignInFailure(err))
+    yield put(signInFailure(err))
   }
 }
 
@@ -28,11 +26,9 @@ export function* signInWithEmail({ payload: { email, password } }) {
     const { user } = yield auth.signInWithEmailAndPassword(email, password)
     const userRef = yield call(createUserProfileDocument, user)
     const userSnapshot = yield userRef.get()
-    yield put(
-      emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-    )
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }))
   } catch (err) {
-    yield put(emailSignInFailure(err))
+    yield put(signInFailure(err))
   }
 }
 
