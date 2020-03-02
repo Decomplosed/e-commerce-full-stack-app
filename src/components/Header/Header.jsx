24e9dc4b45b26e4f355a8033d5585e07f888a9e4
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-02 10:09:10 +0100

    import signOutStart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import { auth } from '../../firebase/firebase.utils'
 
 import { selectCartHidden } from '../../redux/cart/cart.selectors'
 import { selectCurrentUser } from '../../redux/user/user.selectors'
+import { signOutStart } from '../../redux/user/user.actions'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import {
