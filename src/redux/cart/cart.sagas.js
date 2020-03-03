c3137b14628b08868d0b399c408999c3edb5738d
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-03 22:41:35 +0100

    created onSignOutSuccess generator

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -3,6 +3,10 @@ import { all, call, takeLatest, put } from 'redux-saga/effects'
 import UserActionTypes from '../user/user.actions'
 import { clearCart } from './cart.actions'
 
+export function* onSignOutSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, )
+}
+
 export function* cartSagas() {
   yield all([])
 }
