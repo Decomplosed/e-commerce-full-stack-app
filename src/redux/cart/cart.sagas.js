438353a705e6a6fadc4c8d57d9c3658a55954888
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-03 22:40:47 +0100

    imported clearCart

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -1,7 +1,8 @@
 import { all, call, takeLatest, put } from 'redux-saga/effects'
 
 import UserActionTypes from '../user/user.actions'
+import { clearCart } from './cart.actions'
 
 export function* cartSagas() {
-  yield(all([]))
+  yield all([])
 }
