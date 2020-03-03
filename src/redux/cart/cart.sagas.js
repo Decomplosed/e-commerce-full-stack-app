7824349867ab7d4fc9c43e894cd3e6477daba42f
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-03 22:42:33 +0100

    created clearCartSignOut generator

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -3,8 +3,12 @@ import { all, call, takeLatest, put } from 'redux-saga/effects'
 import UserActionTypes from '../user/user.actions'
 import { clearCart } from './cart.actions'
 
+export function* clearCartSignOut() {
+  yield put(clearCart())
+}
+
 export function* onSignOutSuccess() {
-  yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, )
+  yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS)
 }
 
 export function* cartSagas() {
