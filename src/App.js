7c7b3adc48cfbb5f7e22a1bc9734c734b82735d2
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-04 13:07:02 +0100

    imported useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Homepage from './pages/homepage/Homepage'
 import ShopPage from './pages/shop/ShopPage'
 import CheckoutPage from './pages/checkout/Checkout'
