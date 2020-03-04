f62e3b8f870e918cf06a10da2b58802a52a2def9
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-04 14:13:22 +0100

    initialize app and set port

--- server.js
+++ server.js
@@ -3,4 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-if(process.env.NODE_ENV !== 'production') require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
