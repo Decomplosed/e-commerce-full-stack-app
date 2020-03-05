dc3f8b01fb825d35dd38b25725e7aba2728752ac
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-05 10:01:21 +0100

    changed the Collection to use context and wrapped it inside CollectionsContext.Consumer

--- client/src/pages/collection/Collection.jsx
+++ client/src/pages/collection/Collection.jsx
@@ -7,17 +7,24 @@ import CollectionsContext from '../../contexts/collections/collections.context'
 
 import './Collection.styles.scss'
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection
+const CollectionPage = ({ match }) => {
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <CollectionsContext.Consumer>
+      {collections => {
+        const collection = collections[match.params.collectionId]
+        const { title, items } = collection
+        return (
+          <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+              {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        )
+      }}
+    </CollectionsContext.Consumer>
   )
 }
 
