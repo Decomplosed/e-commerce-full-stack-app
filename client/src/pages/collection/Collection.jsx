300783f92e277ca846c7a216d6c3ceba89a69435
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-06 13:35:44 +0100

    removed context from Collection component

--- client/src/pages/collection/Collection.jsx
+++ client/src/pages/collection/Collection.jsx
@@ -1,28 +1,24 @@
 import React from 'react'
 import CollectionItem from '../../components/CollectionItem/CollectionItem'
-import CollectionsContext from '../../contexts/collections/collections.context'
 
 import './Collection.styles.scss'
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection
   return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId]
-        const { title, items } = collection
-        return (
-          <div className='collection-page'>
-            <h2 className='title'>{title}</h2>
-            <div className='items'>
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        )
-      }}
-    </CollectionsContext.Consumer>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default CollectionPage
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage)
