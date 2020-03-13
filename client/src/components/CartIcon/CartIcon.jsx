f6fd8b9b7f880e482b7fc376716828af30ccb642
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-13 10:34:59 +0100

    implemented the styled components to component itself

--- client/src/components/CartIcon/CartIcon.jsx
+++ client/src/components/CartIcon/CartIcon.jsx
@@ -11,10 +11,10 @@ import {
 } from './CartIcon.styles'
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-  <div className='cart-icon' onClick={toggleCartHidden}>
+  <CartContainer onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>{itemCount}</span>
-  </div>
+    <ItemCountContainer>{itemCount}</ItemCountContainer>
+  </CartContainer>
 )
 
 const mapDispatchToProps = dispatch => ({
