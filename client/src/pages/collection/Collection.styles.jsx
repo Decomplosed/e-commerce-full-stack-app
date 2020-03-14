61e6a013c113269f7fbaf021da652bda70812831
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-14 21:48:06 +0100

    moved styles for CollectionTitle

--- client/src/pages/collection/Collection.styles.jsx
+++ client/src/pages/collection/Collection.styles.jsx
@@ -4,3 +4,8 @@ export const CollectionPageContainer = styled.div`
   display: flex;
   flex-direction: column;
 `
+
+export const CollectionTitle = styled.h2`
+  font-size: 38px;
+  margin: 0 auto 30px;
+`
