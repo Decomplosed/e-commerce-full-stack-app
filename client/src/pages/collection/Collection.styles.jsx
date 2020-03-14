93b72c5178e99c715ac22dbfbda6c57a8ce2d031
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-14 21:48:21 +0100

    moved styles for CollectionItemsContainer

--- client/src/pages/collection/Collection.styles.jsx
+++ client/src/pages/collection/Collection.styles.jsx
@@ -9,3 +9,12 @@ export const CollectionTitle = styled.h2`
   font-size: 38px;
   margin: 0 auto 30px;
 `
+
+export const CollectionItemsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+  & > div {
+    margin-bottom: 30px;
+  }
+`
