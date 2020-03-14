5b3a1a722839ea34f05bec180f3408bf9d1d8546
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-14 21:49:58 +0100

    implemented styled components to the Collection page template

--- client/src/pages/collection/Collection.jsx
+++ client/src/pages/collection/Collection.jsx
@@ -13,14 +13,14 @@ import {
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection
   return (
-    <div className='collection-page'>
-      <h2 className='title'>{title}</h2>
-      <div className='items'>
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   )
 }
 
