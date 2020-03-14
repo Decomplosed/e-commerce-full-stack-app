0f89dd46e49a6b7429c3e4b0be1d008b0bdc2753
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-14 22:13:14 +0100

    removed unsued CustomButton import

--- client/src/components/CartDropdown/CartDropdown.jsx
+++ client/src/components/CartDropdown/CartDropdown.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import CustomButton from '../CustomButton/CustomButton'
 import CartItem from '../CartItem/CartItem'
 
 import { selectCartItems } from '../../redux/cart/cart.selectors'
