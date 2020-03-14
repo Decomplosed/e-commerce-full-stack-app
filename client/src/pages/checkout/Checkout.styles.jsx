a6eb962c854ef7a9ccf26d09c63c34402c9e9686
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-14 21:37:49 +0100

    moved styles for CheckoutPageContainer

--- client/src/pages/checkout/Checkout.styles.jsx
+++ client/src/pages/checkout/Checkout.styles.jsx
@@ -1,1 +1,14 @@
 import styled from 'styled-components'
+
+export const CheckoutPageContainer = styled.div`
+  width: 55%;
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px auto 0;
+  button {
+    margin-left: auto;
+    margin-top: 50px;
+  }
+`
