4a3dad902d7ca5c2b6d544fdf5b8251703471659
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-14 22:47:18 +0100

    import SignInAndSignUp using lazy

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import { GlobalStyle } from './global.styles'
 const Homepage = lazy(() => import('./pages/homepage/Homepage'))
 const ShopPage = lazy(() => import('./pages/shop/ShopPage'))
 const CheckoutPage = lazy(() => import('./pages/checkout/Checkout'))
+const SignInAndSignUp = lazy(() => import('./pages/sign-in-and-sing-up/SignInAndSignUp'))
 
 const App = ({ checkUserSession, currentUser }) => {
   useEffect(() => {
