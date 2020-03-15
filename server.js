7a106bb12833bff36296d3f923c65a39e9b002a5
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-15 14:23:27 +0100

    app use enforce and cors

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
+app.use(cors)
 app.use(compression())
 
 if (process.env.NODE_ENV === 'production') {
