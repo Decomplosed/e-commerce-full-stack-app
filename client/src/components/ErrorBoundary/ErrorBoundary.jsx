95792814b9d87adf780fb3dc5b7ef2ecf39c1a65
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-15 11:34:41 +0100

    defined getDerivedStateFromError which returns hasErrored with true value

--- client/src/components/ErrorBoundary/ErrorBoundary.jsx
+++ client/src/components/ErrorBoundary/ErrorBoundary.jsx
@@ -1,3 +1,15 @@
 import React from 'react'
 
-class ErrorBoundary extends React.Component {}
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      hasErrored: false
+    }
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasErrored: true }
+  }
+}
