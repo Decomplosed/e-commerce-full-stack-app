170a57476c6a7b05627eff3bf603e9908ddc4e5b
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-15 12:59:07 +0100

    moved styles for ItemDetailsContainer

--- client/src/components/CartItem/CartItem.styles.jsx
+++ client/src/components/CartItem/CartItem.styles.jsx
@@ -10,3 +10,12 @@ export const CartItemContainer = styled.div`
 export const CartItemImage = styled.img`
   width: 30%;
 `
+
+export const ItemDetailsContainer = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 10px 20px;
+`
